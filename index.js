de43ef7453e57f8cf2bbf7da2a50c633a7438085
kysuukhe <kysuukhe@gmail.com>
2020-11-19 09:21:38 +0100

    code rearrangement

--- index.js
+++ index.js
@@ -15,45 +15,40 @@ const reddit = new Reddit({
 const articles = JSON.parse(fs.readFileSync('./articles.json', 'utf-8'));
 const subreddits = JSON.parse(fs.readFileSync('./subreddits.json', 'utf-8'));
 
-client.login(process.env.TOKEN)
-    .catch(console.error);
+client.login(process.env.TOKEN).catch(console.error);
 
 client.on('ready', async () => {
-    const guild = await client.guilds.fetch(process.env.GUILD);
+    const guild = await client.guilds.fetch(process.env.GUILD).catch(console.error);
     setInterval(async () => {
         for (const subreddit of subreddits) {
-            try {
-                const response = await reddit.get(`/r/${subreddit}/new`);
-                const channels = client.channels.cache;
-                let channel = channels.find(channel => channel.name === subreddit.toLowerCase());
-                if (!channel) {
-                    channel = await guild.channels.create(subreddit);
-                }
-                for (const children of response.data.children) {
-                    const article = children.data;
-                    if (!articles[article.id]) {
-                        articles[article.id] = true;
-                        fs.writeFileSync('./articles.json', JSON.stringify(articles));
-                        if (article.url.includes('.jpg') || article.url.includes('.jpeg') || article.url.includes('.png')) {
-                            channel.send({
-                                files: [article.url]
-                            });
-                        }
-                        if (article.url.includes('.gif')) {
-                            channel.send(article.url);
-                        }
-                        if (article.url.includes('redgifs')) {
-                            const path = await redgifs(article.url, article.id, './redgifs/');
-                            await channel.send({
-                                files: [path.HQ]
-                            });
-                            fs.unlinkSync(path.HQ);
-                            fs.unlinkSync(path.LQ);
-                        }
+            const response = await reddit.get(`/r/${subreddit}/new`).catch(console.error);
+            const channels = client.channels.cache;
+            let channel = channels.find(channel => channel.name === subreddit.toLowerCase());
+            if (!channel) {
+                channel = await guild.channels.create(subreddit).catch(console.error);
+            }
+            for (const children of response.data.children) {
+                const article = children.data;
+                if (!articles[article.id]) {
+                    articles[article.id] = true;
+                    fs.writeFileSync('./articles.json', JSON.stringify(articles));
+                    if (article.url.includes('.jpg') || article.url.includes('.jpeg') || article.url.includes('.png')) {
+                        channel.send({
+                            files: [article.url]
+                        });
+                    }
+                    if (article.url.includes('.gif')) {
+                        channel.send(article.url);
+                    }
+                    if (article.url.includes('redgifs')) {
+                        const path = await redgifs(article.url, article.id, './redgifs/').catch(console.error);
+                        await channel.send({
+                            files: [path.HQ]
+                        }).catch(console.error);
+                        fs.unlinkSync(path.HQ);
+                        fs.unlinkSync(path.LQ);
                     }
                 }
-            } catch {
-                console.error(`an error occurred with the subreddit ${subreddit}`);
             }
         }
     }, 5000);
