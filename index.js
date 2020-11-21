703b42291aa93df38fcd849e97d3464a0eb0f562
kysuukhe <kysuukhe@gmail.com>
2020-11-21 03:27:13 +0100

    fetch now hot and new

--- index.js
+++ index.js
@@ -19,49 +19,53 @@ const subreddits = JSON.parse(fs.readFileSync('./subreddits.json', 'utf-8'));
 
 client.login(process.env.TOKEN).catch(console.error);
 
+const types = ['hot','new'];
+
 client.on('ready', async () => {
     const guild = await client.guilds.fetch(process.env.GUILD).catch(console.error);
 
     setInterval(async () => {
         for (const subreddit of subreddits) {
-            const response = await reddit.get(`/r/${subreddit}/hot`).catch(console.error);
-            let channel = client.channels.cache.find(channel => channel.name === subreddit.toLowerCase());
+            for (const type of types) {
+                const response = await reddit.get(`/r/${subreddit}/${type}`).catch(console.error);
+                let channel = client.channels.cache.find(channel => channel.name === subreddit.toLowerCase());
 
-            if (!channel) {
-                channel = await guild.channels.create(subreddit).catch(console.error);
-            }
+                if (!channel) {
+                    channel = await guild.channels.create(subreddit).catch(console.error);
+                }
 
-            for (const children of response.data.children) {
-                const article = children.data;
+                for (const children of response.data.children) {
+                    const article = children.data;
 
-                if (!articles[article.id]) {
-                    articles[article.id] = true;
-                    fs.writeFileSync('./articles.json', JSON.stringify(articles));
+                    if (!articles[article.id]) {
+                        articles[article.id] = true;
+                        fs.writeFileSync('./articles.json', JSON.stringify(articles));
 
-                    if (article.url.includes('.jpg') || article.url.includes('.jpeg') || article.url.includes('.png')) {
-                        channel.send({ files: [article.url] }).catch(console.error);
-                    }
+                        if (article.url.includes('.jpg') || article.url.includes('.jpeg') || article.url.includes('.png')) {
+                            channel.send({ files: [article.url] }).catch(console.error);
+                        }
 
-                    if (article.url.includes('.gif')) {
-                        channel.send(article.url).catch(console.error);
-                    }
+                        if (article.url.includes('.gif')) {
+                            channel.send(article.url).catch(console.error);
+                        }
 
-                    if (article.url.includes('redgifs')) {
-                        const path = await redgifs(article.url, article.id, './redgifs/').catch(console.error);
-                        await channel.send({ files: [path.HQ] })
-                            .catch(async (error) => {
-                                switch (error.code) {
-                                    case '40005':
-                                        channel.send(path.LQ).catch(console.error);
-                                        break;
-                                    default:
-                                        console.error(error);
-                                        break;
-                                }
-                            });
+                        if (article.url.includes('redgifs')) {
+                            const path = await redgifs(article.url, article.id, './redgifs/').catch(console.error);
+                            await channel.send({ files: [path.HQ] })
+                                .catch(async (error) => {
+                                    switch (error.code) {
+                                        case '40005':
+                                            channel.send(path.LQ).catch(console.error);
+                                            break;
+                                        default:
+                                            console.error(error);
+                                            break;
+                                    }
+                                });
 
-                        fs.unlinkSync(path.HQ);
-                        fs.unlinkSync(path.LQ);
+                            fs.unlinkSync(path.HQ);
+                            fs.unlinkSync(path.LQ);
+                        }
                     }
                 }
             }
